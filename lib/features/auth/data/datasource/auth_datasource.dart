630cd3e830e351417e696cdc5159dde09dde775b
= <=>
2026-05-19 06:48:23 +0530

    Created a UserModel in features/auth/data/models

--- lib/features/auth/data/datasource/auth_datasource.dart
+++
@@ -1,6 +0,0 @@
-import 'package:fpdart/fpdart.dart';
-
-abstract interface class AuthDatasource {
-  Future<String> login(String email, String password);
-  Future<String> signup(String email, String password);
-}
---
+++ lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class UserModel {
+  final String id;
+  final String email;
+  final String name;
+  final String department;
+  final String phoneNo;
+  final String userRole;
+
+  UserModel({
+    required this.id,
+    required this.email,
+    required this.name,
+    required this.department,
+    required this.phoneNo,
+    required this.userRole,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'email': email,
+      'name': name,
+      'department': department,
+      'phoneNo': phoneNo,
+      'userRole': userRole,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'] as String,
+      email: map['email'] as String,
+      name: map['name'] as String,
+      department: map['department'] as String,
+      phoneNo: map['phoneNo'] as String,
+      userRole: map['userRole'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}
