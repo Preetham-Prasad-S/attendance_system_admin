6ab846f16a6c3e59fe3368582cbeed3c9749b8e3
Preetham-Prasad-S <preetham2005105@gmail.com>
2026-05-21 12:21:47 +0530

    Layout Finished , For Navigation Rail and The Main page in the baseScreen Desktop

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const lightScaffoldColor = Color.fromARGB(255, 235, 235, 235);
+  static const whiteColor = Color.fromARGB(255, 255, 255, 255);
+}
--- lib/core/screens/base_desktop_screen.dart
+++ lib/core/screens/base_desktop_screen.dart
@@ -1,3 +1,4 @@
+import 'package:attendance_system_admin/core/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class BaseDesktopScreen extends StatelessWidget {
@@ -8,11 +9,20 @@ class BaseDesktopScreen extends StatelessWidget {
     // final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
+      backgroundColor: AppColors.lightScaffoldColor,
       body: Row(
         children: [
-          Container(height: double.infinity, width: 300, color: Colors.amber),
-          IndexedStack(
-            children: [Container(color: Colors.blue, width: 200, height: 200)],
+          Container(
+            margin: EdgeInsets.all(20),
+            height: double.infinity,
+            width: 350,
+            decoration: BoxDecoration(color: AppColors.whiteColor),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: IndexedStack(children: [Container(color: Colors.blue)]),
+            ),
           ),
         ],
       ),
