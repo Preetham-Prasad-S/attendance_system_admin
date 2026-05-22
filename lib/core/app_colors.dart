952f7815d802ede2d3a6780f6fc5eeb0e0eef277
Preetham-Prasad-S <preetham2005105@gmail.com>
2026-05-22 23:27:51 +0530

    Added A smaller icon for the header in the navrail in the base screen

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   AppColors._();
-  static const lightScaffoldColor = Color.fromARGB(255, 235, 235, 235);
+  static const lightScaffoldColor = Color.fromARGB(255, 243, 243, 243);
   static const whiteColor = Color.fromARGB(255, 255, 255, 255);
 }
--- lib/core/screens/base_desktop_screen.dart
+++ lib/core/screens/base_desktop_screen.dart
@@ -1,5 +1,6 @@
 import 'package:attendance_system_admin/core/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BaseDesktopScreen extends StatelessWidget {
   const BaseDesktopScreen({super.key});
@@ -12,17 +13,55 @@ class BaseDesktopScreen extends StatelessWidget {
       backgroundColor: AppColors.lightScaffoldColor,
       body: Row(
         children: [
-          Container(
-            margin: EdgeInsets.all(20),
-            height: double.infinity,
-            width: 350,
-            decoration: BoxDecoration(color: AppColors.whiteColor),
-          ),
+          CustomNavRail(),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(20),
-              child: IndexedStack(children: [Container(color: Colors.blue)]),
+              child: IndexedStack(
+                children: [
+                  Container(color: const Color.fromARGB(255, 255, 255, 255)),
+                ],
+              ),
             ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomNavRail extends StatelessWidget {
+  const CustomNavRail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      padding: EdgeInsets.all(10),
+      height: double.infinity,
+      width: 350,
+      decoration: BoxDecoration(
+        color: AppColors.whiteColor,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      child: Column(
+        children: [
+          Row(
+            spacing: 10,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ImageIcon(AssetImage("icon-small.png")),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Attendance Manager",
+                    style: GoogleFonts.rubik(fontSize: 20),
+                  ),
+                  Text("Admin Console", style: GoogleFonts.rubik()),
+                ],
+              ),
+            ],
           ),
         ],
       ),
