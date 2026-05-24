432cf880e1d442e1e7ea8be7abefcca9fb458d19
= <=>
2026-05-24 12:23:43 +0530

    Started working on the Employee screen

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   AppColors._();
-  static const lightScaffoldColor = Color.fromARGB(255, 243, 243, 243);
+  static const lightScaffoldColor = Color.fromARGB(255, 243, 248, 255);
   static const whiteColor = Color.fromARGB(255, 255, 255, 255);
   static const blueColor = Color.fromRGBO(7, 129, 229, 1);
   static const navButtonBackgroundColor = Color.fromARGB(16, 33, 149, 243);
--- lib/core/screens/base_desktop_screen.dart
+++ lib/core/screens/base_desktop_screen.dart
@@ -1,7 +1,6 @@
 import 'package:attendance_system_admin/core/app_colors.dart';
+import 'package:attendance_system_admin/core/widgets/custom_navigation_rail_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:ionicons/ionicons.dart';
 
 class BaseDesktopScreen extends StatelessWidget {
   const BaseDesktopScreen({super.key});
@@ -14,15 +13,11 @@ class BaseDesktopScreen extends StatelessWidget {
       backgroundColor: AppColors.lightScaffoldColor,
       body: Row(
         children: [
-          CustomNavRail(),
+          CustomNavigationRailWidget(),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(20),
-              child: IndexedStack(
-                children: [
-                  Container(color: const Color.fromARGB(255, 255, 255, 255)),
-                ],
-              ),
+              child: IndexedStack(children: [(EmployeeScreen())]),
             ),
           ),
         ],
@@ -31,96 +26,13 @@ class BaseDesktopScreen extends StatelessWidget {
   }
 }
 
-class CustomNavRail extends StatelessWidget {
-  const CustomNavRail({super.key});
+class EmployeeScreen extends StatelessWidget {
+  const EmployeeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(20),
-      padding: EdgeInsets.all(10),
-      height: double.infinity,
-      width: 350,
-      decoration: BoxDecoration(
-        color: AppColors.whiteColor,
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-      ),
-      child: Column(
-        children: [
-          SizedBox(height: 10),
-          // Nav Title
-          Row(
-            spacing: 10,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              DecoratedBox(
-                decoration: BoxDecoration(
-                  color: AppColors.blueColor,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                child: ImageIcon(
-                  AssetImage("icon-small.png"),
-                  size: 50,
-                  color: AppColors.whiteColor,
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Attendance Manager",
-                    style: GoogleFonts.quicksand(
-                      fontSize: 23,
-                      color: AppColors.blueColor,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    "Admin Console",
-                    style: GoogleFonts.quicksand(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          const SizedBox(height: 50),
-
-          // Employee Nav Button
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            decoration: BoxDecoration(
-              // border: Border.all(
-              //   color: const Color.fromARGB(29, 7, 129, 229),
-              //   width: 2,
-              // ),
-              borderRadius: BorderRadius.all(Radius.circular(15)),
-              color: AppColors.navButtonBackgroundColor,
-            ),
-            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Icon(
-                  Ionicons.people_outline,
-                  size: 25,
-                  color: AppColors.blueColor,
-                ),
-                SizedBox(width: 10),
-                Text(
-                  "Employees",
-                  style: GoogleFonts.quantico(
-                    fontSize: 18,
-                    color: AppColors.blueColor,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+    return Column(
+      children: [SizedBox(height: 30, child: Row(children: []))],
     );
   }
 }
