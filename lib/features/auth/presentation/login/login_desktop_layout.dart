f7861a479de9982d81790b01186617a988bc8f6b
= <=>
2026-05-25 11:49:09 +0530

    Added some folder for clean structure in auth/presentation

--- lib/features/auth/presentation/login/login_desktop_layout.dart
+++ lib/features/auth/presentation/login/login_desktop_layout.dart
@@ -2,7 +2,6 @@ import 'package:attendance_system_admin/core/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:ionicons/ionicons.dart';
 
 class LoginDesktopLayout extends StatefulWidget {
   const LoginDesktopLayout({super.key});
