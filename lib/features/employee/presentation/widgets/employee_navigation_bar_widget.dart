bb3bc674d199980123290706f2599091e630d4a1
Preetham-Prasad-S <preetham2005105@gmail.com>
2026-05-26 11:47:34 +0530

    Organized the EmployeeNavigationBarWidget in lib/features/employee/presentation/widgets/employee_navigation_widget.dart

--- lib/features/employee/presentation/widgets/employee_navigation_bar_widget.dart
+++ lib/features/employee/presentation/widgets/employee_navigation_bar_widget.dart
@@ -19,82 +19,98 @@ class EmployeeNavigationBarWidget extends StatelessWidget {
           padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
           child: Row(
             children: [
-              SizedBox(
-                width: 400,
-                child: TextField(
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(0),
-                    hintStyle: GoogleFonts.quicksand(
-                      fontWeight: FontWeight.w500,
-                      color: const Color.fromARGB(207, 127, 152, 187),
-                    ),
-                    hintText: "Search users, department...",
-                    prefixIcon: Icon(
-                      Ionicons.search,
-                      color: const Color.fromARGB(160, 127, 152, 187),
-                    ),
-                    filled: true,
-                    fillColor: AppColors.lightScaffoldColor,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      borderSide: BorderSide(
-                        width: 1.5,
-                        color: const Color.fromARGB(255, 177, 197, 225),
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      borderSide: BorderSide(
-                        width: 1.5,
-                        color: const Color.fromARGB(255, 177, 197, 225),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Spacer(),
+              const SearchBarWidget(),
+              const Spacer(),
               IconButton(
                 onPressed: () {},
                 icon: Icon(Ionicons.notifications_outline),
               ),
-              SizedBox(width: 20),
+              const SizedBox(width: 20),
               IconButton(
                 onPressed: () {},
                 icon: Icon(Ionicons.help_circle_outline),
               ),
-              SizedBox(width: 20),
-              VerticalDivider(),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: const Color.fromARGB(255, 219, 234, 255),
-                    ),
-                    SizedBox(width: 10),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Preetham Prasad S",
-                          style: GoogleFonts.quicksand(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          "Super User",
-                          style: GoogleFonts.quicksand(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 12,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+              const SizedBox(width: 20),
+              const VerticalDivider(),
+              const ProfileCardWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileCardWidget extends StatelessWidget {
+  const ProfileCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundColor: const Color.fromARGB(255, 219, 234, 255),
+          ),
+          SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Preetham Prasad S",
+                style: GoogleFonts.quicksand(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Super User",
+                style: GoogleFonts.quicksand(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12,
+                  color: Colors.grey,
                 ),
               ),
             ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchBarWidget extends StatelessWidget {
+  const SearchBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 400,
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          hintStyle: GoogleFonts.quicksand(
+            fontWeight: FontWeight.w500,
+            color: const Color.fromARGB(207, 127, 152, 187),
+          ),
+          hintText: "Search users, department...",
+          prefixIcon: Icon(
+            Ionicons.search,
+            color: const Color.fromARGB(160, 127, 152, 187),
+          ),
+          filled: true,
+          fillColor: AppColors.lightScaffoldColor,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            borderSide: BorderSide(
+              width: 1.5,
+              color: const Color.fromARGB(255, 177, 197, 225),
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            borderSide: BorderSide(
+              width: 1.5,
+              color: const Color.fromARGB(255, 177, 197, 225),
+            ),
           ),
         ),
       ),
