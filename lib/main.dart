c4a6b1f549fe9da8950e579335f21bdd5cd0916c
= <=>
2026-06-07 21:07:16 +0530

    Clean up auth textfield widget and update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:attendance_system_admin/features/auth/presentation/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
